f87dfcb932285376a7b0ff4cf70cc0761cd5d58e
RG <shh-prc@qq.com>
2020-08-17 21:21:25 +0800

    testing binarySearch

--- RG_Algorithm_Template/main.cpp
+++ RG_Algorithm_Template/main.cpp
@@ -20,11 +20,19 @@ void bubbleSortTest() {
 	int  a[7] = { 7,131,1445,132,90,99,1 };
 }
 
+void isInArray(int ele, int result) {
+	if (result == -1)
+		printf("element %d is not in the array!\n", ele);
+	else
+		printf("element %d is in the array, index is %d !", ele, result);
+}
 
 void binarySearchTest() {
 	int a[7] = { 7,131,1445,132,90,99,1 };
 	insertSort(a, 7, true);
-	if(binarySearch())
+	isInArray(99, binarySearch(a, 7, 99));
+	isInArray(999, binarySearch(a, 7, 999));
+
 	
 }
 

5e7ddb48277060188389d8b5183ab270d19c3234
RG <shh-prc@qq.com>
2020-08-17 21:01:25 +0800

    adding binarySearch

--- RG_Algorithm_Template/RG_Algorithm.h
+++ RG_Algorithm_Template/RG_Algorithm.h
@@ -14,3 +14,5 @@ void insertSort(int a[], int len, bool reverse = false);
 // 算法笔记 书上给的 生成全排列 的实现方法 -- 对应经典问题 n皇后问题
 void generateP(int index, int n, int p[], int hashTabe[], int &count);
 int showFullPermutation(int n);
+
+int binarySearch(int a[], int element);
